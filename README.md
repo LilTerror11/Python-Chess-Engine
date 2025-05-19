85d8958256e400a34b10d7307a4141b726c8db51
LilTerror11 <alexander.lilterror11@gmail.com>
2025-05-19 13:21:01 +0930

    Updated README.md adding more information, added functions.py (Empty), added more classes and started on config reading, made small changes to config format

--- README.md
+++ README.md
@@ -9,9 +9,23 @@ This project contains my 3rd attempt at making chess with python, the other 2 pr
 * config.json - The config for the pieces, this is used to make a scafold of the basic peace movement, the board setup, and most likely debuging logging
 * Config Format.json - This is a schema for the config.json format, I'm not the best at schemas, so it's not perfect, but ok
 
-## Files to be added
-* Assets/* - this will contain all the images, and will load all the images in this directory into a dictionary that contains all the images, in the format of Assets["directory"]["directory or filename"]...
+### This file has been added
 * functions.py - This will contain all the functions for the events, which will be added to a dictinary, most likely called "EventHandlers", and the key, (provided in the config) will be used to choose the function
 
+
+## Files to be added
+* Assets/* - this will contain all the images, and will load all the images in this directory into a dictionary that contains all the images, in the format of Assets["directory"]["directory or filename"]...
+    #### This has been changed to Assets["relative path"]
+
 ## Events
-* :P i'll do this later
+#### There are 10 different events, 6 of these are in 'move \ take' pairs
+A 'Child' event is a event that is fed into the 'Parent' event
+### Paired Events
+- #### move / take
+   - This event is called whenever a piece is selected, this event manages all the squares that can be taken / moved to
+  - #### These are child events of \'select\' and \'on_event\'
+  - #### These events are technically parent events of \'tile_move\' and \'tile_take\' respectively
+- #### tile_move / tile_take
+  - These events are called on move / take generation, for each tile in the move / take, this event is called to decide if the move / take should continue, this event
+  - #### These events are technically child events for \'move\' and \'take\' respectively and child events of \'on_event\'
+  :P i'll continue this later
--- classes.py
+++ classes.py
@@ -1,33 +1,30 @@
-from typing import Any
-from typing import Iterable
+import copy
+import warnings
+from collections.abc import Callable, Sequence
+from typing import Iterable, Any, Protocol, runtime_checkable, Optional
+from warnings import warn
+
+from pygame import Vector2, Surface
+
+# ------- ERRORS -------
 
 
-class Piece:
-    def __init__(self):
-        ...
+class NoBoardException(Exception):
+    ...
+
+class ConfigException(Exception):
+    ...
+
+class ConfigWarning(UserWarning):
+    ...
+
+class ConfigLoggingWarning(UserWarning):
+    ...
 
 
-class Board:
+# ------------ TYPES / CLASSES ------------
 
-    __board: list[list[Piece]] = [[None] * 8] * 8
-    board_init: list[list[int]]
-
-    def gen_board(self):
-        self.board_init: list[list[int]]
-        for x in range(len(self.board_init)):
-            row = self.board_init[x]
-            for y in range(len(row)):
-                piece = row[y]
-                self.__board[x][y] = Piece()
-
-    def __init__(self, board: list[list[int]]=None):
-        if board is None:
-            board = [[0] * 8] * 8
-        self.board_init = board
-        self.gen_board()
-
-    def get_piece_at(self, x, y) -> Piece:
-        return self.__board[x][y]
+coordinate = Vector2 | Sequence[int]
 
 
 class AttributeDict(dict):
@@ -50,39 +47,229 @@ class AttributeDict(dict):
 
     def __setitem__(self, key, value):
         super().__setattr__(key, value)
-        print(self)
 
     def __setattr__(self, key, value):
         super().__setitem__(key, value)
 
+# "move"
+# "take"
+# "setup"
+# "select"
+# "tile_move"
+# "tile_take"
+# "on_move"
+# "on_take"
+# "on_event"
+
+class PieceEvents:
+    def __init__(self, move = "default.move", take = "default.take", setup = "default.null",
+                 tile_move = "default.tile_move", tile_take = "default.tile_take", on_attack = "default.null",
+                 on_move = "default.null", on_taken = "default.null", on_event = "default.null"):
+
+        def list_str(val: list | str):
+            if isinstance(val, str):
+                return [val]
+            return val
+
+        self.move = list_str(move)
+        self.take = list_str(take)
+        self.setup = list_str(setup)
+
+        self.tile_move = list_str(tile_move)
+        self.tile_take = list_str(tile_take)
+
+        self.on_move = list_str(on_move)
+        self.on_taken = list_str(on_taken)
+        self.on_attack = list_str(on_attack)
+        self.on_event = list_str(on_event)
+
+
+@runtime_checkable
+class HasMoveData(Protocol):
+    def get_move_data(self) -> None:
+        ...
+
+class GeneratedPiece:
+    name: Optional[str] = None
+    blank: Optional[bool] = None
+
+    moves: Optional[list[list[int, int, int]]] = None
+    attack: Optional[list[list[int, int, int]]] = None
+
+    events: Optional[AttributeDict[str]] = None
+
+    display: Optional[AttributeDict[str, str | Surface]] = None
+
+    __pieces = {}
+    __piece_ids = {}
+
+    def __init__(self, config: AttributeDict | dict, piece_id: str):
+        if isinstance(config, dict):
+            config = AttributeDict(config)
+
+        keys = list(config.keys())
+        self.keys = keys
+
+        self.blank = False
+
+        if len(keys) == 0:
+            warn(f"Piece \"id{piece_id}\" has no Config", ConfigWarning, stacklevel=3)
+            warn(f"Piece \"id{piece_id}\" has been blanked", ConfigLoggingWarning, stacklevel=3)
+            self.blank = True
+
+        if keys.__contains__("blank"):
+            self.blank = config.blank
+        if not self.blank:
+            print(piece_id)
+            if keys.__contains__("name"):
+                self.name = config.name
+            else:
+                raise ConfigException(f"Non blank piece \"id{piece_id}\" doesn't have \"name\" tag")
+            if keys.__contains__("moves"):
+                self.moves = config.moves
+            else:
+                warn(f"Piece \"{self.name}\" is missing the \"moves\" tag", ConfigWarning, stacklevel=3)
+                warn(f"Piece \"{self.name}\" has had its moves blanked", ConfigLoggingWarning, stacklevel=3)
+                self.moves = []
+            if keys.__contains__("takes"):
+                self.takes = config.takes
+            else:
+                warn(f"Piece \"{self.name}\" is missing the \"takes\" tag", ConfigWarning, stacklevel=3)
+                warn(f"Piece \"{self.name}\" has had its takes blanked", ConfigLoggingWarning, stacklevel=3)
+                self.takes = []
+            if keys.__contains__("icon"):
+                icon_config = config.icon.display
+                if icon_config.type == "text":
+                    if isinstance(icon_config.value, dict):
+                        self.display = AttributeDict(icon_config.value)
+                    elif isinstance(icon_config.value, str):
+                        self.display = AttributeDict({"black": icon_config.value, "white": icon_config.value})
+                elif icon_config.type == "image":
+                    self.display = AttributeDict(icon_config.value)
+            else:
+                warn(f"Piece \"{self.name}\" is missing the \"icon\" tag", ConfigWarning, stacklevel=3)
+                warn(f"Piece \"{self.name}\" has had its display be generated ({self.name[0].upper()})", ConfigLoggingWarning, stacklevel=3)
+                self.display = AttributeDict({"black": self.name.upper()[0], "white": self.name.upper()[0]})
+        GeneratedPiece.__pieces[piece_id] = self
+
+    def generate_piece(self, assets, events):
+        ...
+
+    @staticmethod
+    def get_pieces():
+        return copy.copy(GeneratedPiece.__pieces)
+
+    @staticmethod
+    def get_piece_ids():
+        return copy.copy(GeneratedPiece.__piece_ids)
+
+
+class Piece:
+    def __init__(self, events, ):
+        ...
+
+
+class Board:
+
+    __board: list[list[Piece]] = [[None] * 8] * 8
+    board_init: list[list[int]]
+    __assets: dict = None
+    __events: dict = None
+
+    def gen_board(self):
+        self.board_init: list[list[int]]
+        for x in range(len(self.board_init)):
+            row = self.board_init[x]
+            for y in range(len(row)):
+                piece = row[y]
+                self.__board[x][y] = None
+
+    def __init__(self, board: list[list[int]]=None):
+        if board is None:
+            board = [[0] * 8] * 8
+        self.board_init = board
+        self.gen_board()
+
+    def get_piece_at(self, x: coordinate | int, y: Optional[int] = None) -> Piece:
+        if y:
+            return self.__board[x][y]
+        else:
+            return self.__board[x[0]][x[1]]
+
+    def spawn_piece(self, piece: str | GeneratedPiece, x: int | coordinate, y: Optional[int] = None):
+        if y is None:
+            x, y = x
+
+        if not isinstance(piece, GeneratedPiece):
+            generated_piece: GeneratedPiece = GeneratedPiece.get_pieces()[piece]
+            generated_piece.generate_piece(self.__assets, self.__events)
+
+    @staticmethod
+    def attach_events(events):
+        Board.__events = events
+
+    @staticmethod
+    def get_events():
+        return copy.copy(Board.__assets)
+
+    @staticmethod
+    def attach_assets(assets):
+        Board.__assets = assets
+
+    @staticmethod
+    def get_assets():
+        return copy.copy(Board.__assets)
+
 
 # ------- EVENTS -------
 
-class TileMoveEvent:
+# Base Event class, for global values in events
+class Event:
+    __board: Board = None
+
+    @staticmethod
+    def set_board(board: Board):
+        Event.__board = board
+
+    @staticmethod
+    def get_board():
+        if Event.__board:
+            return Event.__board
+        else:
+            raise NoBoardException("No Board has been set")
+
+
+class TileMoveEvent(Event):
 
     piece: Piece
     raw_move: list[int, int, int]
-    start: list[int, int]
-    pos: list[int, int]
+    start: coordinate
+    pos: coordinate
+    pre: coordinate
     count: int
     child: Any | None
     parent: Any | None
 
     __length: int = 0
+    board: Board
 
 
     def __init__(self,
                  piece: Piece,
+                 board: Board,
                  raw_move: list[int, int, int],
-                 start: list[int, int],
-                 pos: list[int, int],
+                 start: coordinate,
+                 pos: coordinate,
+                 pre: coordinate,
                  count: int,
-                 parent = None
+                 parent = None,
                  ):
         self.piece = piece
         self.raw_move = raw_move
         self.start = start
+        self.pre = pre
         self.pos = pos
+        self.board = board
         self.count = count
         self.parent = parent
         self.child = None
@@ -118,30 +305,36 @@ class TileMoveEvent:
         return iter([self])
 
 
-class TileTakeEvent:
+class TileTakeEvent(Event):
 
     piece: Piece
     raw_take: list[int, int, int]
-    start: list[int, int]
-    pos: list[int, int]
+    start: coordinate
+    pos: coordinate
+    pre: coordinate
     count: int
     child: Any | None
     parent: Any | None
+    board: Board
 
     __length: int = 0
 
     def __init__(self,
                  piece: Piece,
+                 board: Board,
                  raw_take: list[int, int, int],
-                 start: list[int, int],
-                 pos: list[int, int],
+                 start: coordinate,
+                 pre: coordinate,
+                 pos: coordinate,
                  count: int,
-                 parent = None
+                 parent = None,
                  ):
+        self.board = board
         self.piece = piece
         self.raw_take = raw_take
         self.start = start
         self.pos = pos
+        self.pre = pre
         self.count = count
         self.parent = parent
         self.child = None
@@ -177,7 +370,24 @@ class TileTakeEvent:
         return iter([self])
 
 
-class MoveEvent:
+class MoveData(Event):
+    pos: Vector2
+    piece: Piece
+
+    def __init__(self, piece: Piece | HasMoveData, x: int | coordinate  = None, y: Optional[int] = None):
+        if isinstance(piece, HasMoveData):
+            print(piece.get_move_data())
+        elif isinstance(x, int):
+            if y is None:
+                self.pos = Vector2(x)
+            else:
+                self.pos = Vector2(x, y)
+
+    def get_move_data(self):
+        return self
+
+
+class MoveEvent(Event):
     piece: Piece
     moves: list[TileMoveEvent]
     raw_moves: list[list[int, int, int]]
@@ -188,7 +398,7 @@ class MoveEvent:
         self.raw_moves = raw_moves
 
 
-class TakeEvent:
+class TakeEvent(Event):
     piece: Piece
     moves: list[TileTakeEvent]
     raw_takes: list[list[int, int, int]]
